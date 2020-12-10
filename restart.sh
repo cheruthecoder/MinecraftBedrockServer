7a65012043bc4f51e29727a9c1e39755ffd41465
cheruthecoder <65314900+cheruthecoder@users.noreply.github.com>
2020-12-10 12:18:32 -0500

    Update restart.sh
    Changed a few things for auto restart

--- restart.sh
+++ restart.sh
@@ -3,50 +3,50 @@
 # Minecraft Bedrock Server restart script
 
 # Check if server is started
-if ! screen -list | grep -q "servername"; then
+if ! screen -list | grep -q "nerdcraft"; then
     echo "Server is not currently running!"
-    exit 1
+    /bin/bash ~/BDSX/start.sh
 fi
 
 echo "Sending restart notifications to server..."
 
 # Start countdown notice on server
-screen -Rd servername -X stuff "say Server is restarting in 30 seconds! $(printf '\r')"
+screen -Rd nerdcraft -X stuff "say Server is restarting in 30 seconds! $(printf '\r')"
 sleep 23s
-screen -Rd servername -X stuff "say Server is restarting in 7 seconds! $(printf '\r')"
+screen -Rd nerdcraft -X stuff "say Server is restarting in 7 seconds! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Server is restarting in 6 seconds! $(printf '\r')"
+screen -Rd nerdcraft -X stuff "say Server is restarting in 6 seconds! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Server is restarting in 5 seconds! $(printf '\r')"
+screen -Rd nerdcraft -X stuff "say Server is restarting in 5 seconds! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Server is restarting in 4 seconds! $(printf '\r')"
+screen -Rd nerdcraft -X stuff "say Server is restarting in 4 seconds! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Server is restarting in 3 seconds! $(printf '\r')"
+screen -Rd nerdcraft -X stuff "say Server is restarting in 3 seconds! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Server is restarting in 2 seconds! $(printf '\r')"
+screen -Rd nerdcraft -X stuff "say Server is restarting in 2 seconds! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Server is restarting in 1 second! $(printf '\r')"
+screen -Rd nerdcraft -X stuff "say Server is restarting in 1 second! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Closing server...$(printf '\r')"
-screen -Rd servername -X stuff "stop$(printf '\r')"
+screen -Rd nerdcraft -X stuff "say Closing server...$(printf '\r')"
+screen -Rd nerdcraft -X stuff "stop$(printf '\r')"
 
 echo "Closing server..."
 # Wait up to 30 seconds for server to close
 StopChecks=0
 while [ $StopChecks -lt 30 ]; do
-  if ! screen -list | grep -q "servername"; then
+  if ! screen -list | grep -q "nerdcraft"; then
     break
   fi
   sleep 1;
   StopChecks=$((StopChecks+1))
 done
 
-if screen -list | grep -q "servername"; then
+if screen -list | grep -q "nerdcraft"; then
     # Server still hasn't stopped after 30s, tell Screen to close it
     echo "Minecraft server still hasn't closed after 30 seconds, closing screen manually"
-    screen -S servername -X quit
+    screen -S nerdcraft -X quit
     sleep 10
 fi
 
 # Start server
-/bin/bash dirname/minecraftbe/servername/start.sh
+/bin/bash ~/BDSX/start.sh
