fd6ade5d2312cabadd6a9165ef162a695009fdbc
cheruthecoder <65314900+cheruthecoder@users.noreply.github.com>
2020-12-10 12:06:59 -0500

    Update start.sh
    Made compatible with BDSX and removed autoupdate

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 # Minecraft Bedrock server startup script using screen
 
 # Check if server is already started
-if screen -list | grep -q "servername"; then
-    echo "Server is already started!  Press screen -r servername to open it"
+if screen -list | grep -q "nerdcraft"; then
+    echo "Server is already started!  Press screen -r nerdcraft to open it"
     exit 1
 fi
 
@@ -23,38 +23,14 @@ while [ -z "$DefaultRoute" ]; do
 done
 
 # Change directory to server directory
-cd dirname/minecraftbe/servername
+cd ~/.bds
 
 # Create backup
 if [ -d "worlds" ]; then
-    echo "Backing up server (to minecraftbe/servername/backups folder)"
+    echo "Backing up server (to ~/.bds/backups folder)"
     tar -pzvcf backups/$(date +%Y.%m.%d.%H.%M.%S).tar.gz worlds
 fi
 
-# Retrieve latest version of Minecraft Bedrock dedicated server
-echo "Checking for the latest version of Minecraft Bedrock server ..."
-
-# Test internet connectivity first
-wget --quiet http://www.minecraft.net/ -O /dev/null
-if [ "$?" != 0 ]; then
-    echo "Unable to connect to update website (internet connection may be down).  Skipping update ..."
-else
-    # Download server index.html to check latest version
-    wget -O downloads/version.html https://minecraft.net/en-us/download/server/bedrock/
-    DownloadURL=$(grep -o 'https://minecraft.azureedge.net/bin-linux/[^"]*' downloads/version.html)
-    DownloadFile=$(echo "$DownloadURL" | sed 's#.*/##')
-
-    # Download latest version of Minecraft Bedrock dedicated server if a new one is available
-    if [ -f "downloads/$DownloadFile" ]
-    then
-        echo "Minecraft Bedrock server is up to date..."
-    else
-        echo "New version $DownloadFile is available.  Updating Minecraft Bedrock server ..."
-        wget -O "downloads/$DownloadFile" "$DownloadURL"
-        unzip -o "downloads/$DownloadFile" -x "*server.properties*" "*permissions.json*" "*whitelist.json*"
-    fi
-fi
-
-echo "Starting Minecraft server.  To view window type screen -r servername"
+echo "Starting Minecraft server.  To view window type screen -r nerdcraft"
 echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
-screen -L -Logfile logs/$(date +%Y.%m.%d.%H.%M.%S).log -dmS servername /bin/bash -c "LD_LIBRARY_PATH=dirname/minecraftbe/servername dirname/minecraftbe/servername/bedrock_server"
+screen -L -Logfile logs/$(date +%Y.%m.%d.%H.%M.%S).log -dmS nerdcraft /bin/bash -c "LD_LIBRARY_PATH=~/.bds ~/BDSX/bdsx.sh"
